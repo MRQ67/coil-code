9abfd390ac0939bde31b62f63e4fe4946b16283a
MRQ67 <kasaboody@gmail.com>
2025-10-23 15:05:43 +0300

    feat: Finished setting up frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Handle Monaco Editor's worker files
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "asset/resource",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
